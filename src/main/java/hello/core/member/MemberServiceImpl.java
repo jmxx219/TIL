dbd14349b482f223da6355a5417fdd117d245d5b
Jimin <thswlals0219@gmail.com>
2022-01-15 13:19:59 +0900

    주문과 할인 도메인 개발 및 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // DIP 위반 - 의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존 (문제)
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
