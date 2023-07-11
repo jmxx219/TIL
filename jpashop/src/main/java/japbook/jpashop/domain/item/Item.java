32c9571100826403c04b34377866308f0f4c2d19
Jimin <thswlals0219@gmail.com>
2023-07-11 09:53:12 +0900

    feat: 상품 엔티티 구현

--- jpashop/src/main/java/japbook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/japbook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package japbook.jpashop.domain.item;
 
 import japbook.jpashop.domain.Category;
+import japbook.jpashop.exception.NotEnoughStockException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -30,4 +31,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
