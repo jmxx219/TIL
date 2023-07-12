9e7330666417bc7fbe8354f2abe098311822abb7
Jimin <thswlals0219@gmail.com>
2023-07-12 14:09:47 +0900

    feat: 주문 서비스 구현

--- jpashop/src/main/java/japbook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/japbook/jpashop/domain/OrderItem.java
@@ -8,11 +8,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
