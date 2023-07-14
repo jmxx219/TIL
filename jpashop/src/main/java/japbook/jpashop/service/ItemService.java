804eb501dac4e7e51862c2ac61fa769ecc9f2c69
Jimin <thswlals0219@gmail.com>
2023-07-14 16:39:59 +0900

    feat: 변경 감지와 병합(merge)

--- jpashop/src/main/java/japbook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/japbook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package japbook.jpashop.service;
 
+import japbook.jpashop.domain.item.Book;
 import japbook.jpashop.domain.item.Item;
 import japbook.jpashop.repository.ItemRepository;
 import java.util.List;
@@ -17,6 +18,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+//    public void updateItem(Long itemId, UpdateItemDto itemDto) {
+    public void updateItem(Long itemId, String name, int Price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // param은 준영속 상태의 엔티티
+//        findItem.change(name, price, stockQuantity); // setter를 직접 사용하는 것보다 수정 메서드를 만들어서 사용하는 것이 좋음
+        findItem.setName(name);
+        findItem.setPrice(Price);
+        findItem.setStockQuantity(stockQuantity);
+//        itemRepository.save(findItem); // findItem은 영속 상태이기 때문에 호출하지 않아도 데이터가 변경됨
     }
 
     public List<Item> findItems() {
