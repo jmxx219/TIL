e3cf8c96475634c0263a7b86c4bb17f71e778031
Jimin <thswlals0219@gmail.com>
2023-07-17 11:27:37 +0900

    docs: 영속성 관리 - 내부 동작 방식

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Spring Boot와 JPA
 
 `/orm-jpa` [자바 ORM 표준 JPA](https://github.com/jmxx219/SpringJPA/blob/main/orm-jpa)
+- [JPA 소개](https://github.com/jmxx219/SpringJPA/blob/main/orm-jpa/JPA%20%EC%86%8C%EA%B0%9C.md)
+- [JPA 시작](https://github.com/jmxx219/SpringJPA/blob/main/orm-jpa/ex1-hello-jpa/README.md)
 
 `/jpashop`
 [웹 애플리케이션 개발](https://github.com/jmxx219/SpringJPA/blob/main/jpashop/README.md)
--- orm-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ orm-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,9 @@
 package hellojpa;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 
 public class JpaMain {
 
@@ -18,19 +16,30 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("helloA");
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("helloJPA");
 
-//            em.persist(member);
+            // 영속
+            System.out.println("==== BEFORE ====");
+            em.persist(member); // 1차 캐시에서 저장
+//            em.detach(member); // 영속성 컨텍스트에서 분리
+            System.out.println("==== AFTER ====");
 
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
+            em.flush(); // 영속성 컨텍스트의 쓰기 지연 SQL 저장소의 쿼리를 DB에 전송(플러시 발생) - INSERT 쿼리가 날아감
 
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                            .getResultList();
+            System.out.println("=====================");
 
-            tx.commit();
+            Member findMember = em.find(Member.class, 100L); // 1차 캐시에서 조회
+            member.setName("hello!"); // JPA는 값이 변경되면 변경을 감지해서 커밋되는 시점에 update 쿼리문을 자동으로 날림
+
+            em.detach(member); // 영속 상태의 엔티티를 영속성 컨텍스트에서 분리(준영속 상태)
+
+            System.out.println("findMember.id " + findMember.getId());
+            System.out.println("findMember.name " + findMember.getName());
+
+            tx.commit(); // 커밋하는 시점에 쿼리가 날아감
         } catch (Exception e) {
             tx.rollback();
         } finally {
