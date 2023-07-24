ef05d53d97a7c186ce48008d57c73ec777737af9
Jimin <thswlals0219@gmail.com>
2023-07-24 12:04:51 +0900

    feat: 연관관계 매핑 설정

--- orm-jpa/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ orm-jpa/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 public class JpaMain {
 
@@ -16,6 +18,14 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Order order = new Order();
+//            order.addOrderItem(new OrderItem());
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
 
             tx.commit(); // 커밋하는 시점에 쿼리가 날아감
         } catch (Exception e) {
--- orm-jpa/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ orm-jpa/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,11 +1,14 @@
 package jpabook.jpashop.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -14,12 +17,13 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "MEMBER_ID")
     private Long id;
-
     private String name;
     private String city;
     private String street;
     private String zipcode;
 
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
