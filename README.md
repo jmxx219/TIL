da6fe380efe4986711368a41b3b40e5e846288a6
Jimin <thswlals0219@gmail.com>
2023-08-23 23:00:45 +0900

    feat: 회원 등록 api 구현

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 - [연관관계 매핑](https://github.com/jmxx219/SpringJPA/blob/main/orm-jpa/%EC%97%B0%EA%B4%80%EA%B4%80%EA%B3%84%20%EB%A7%A4%ED%95%91.md)
 - [프록시와 연관관계 관리](https://github.com/jmxx219/SpringJPA/blob/main/orm-jpa/%ED%94%84%EB%A1%9D%EC%8B%9C%EC%99%80%20%EC%97%B0%EA%B4%80%EA%B4%80%EA%B3%84%20%EA%B4%80%EB%A6%AC.md#%ED%94%84%EB%A1%9D%EC%8B%9C)
 - [값 타입](https://github.com/jmxx219/SpringJPA/blob/main/orm-jpa/%EA%B0%92%20%ED%83%80%EC%9E%85.md)
+- [객체지향 쿼리 언어](https://github.com/jmxx219/SpringJPA/blob/main/orm-jpa/%EA%B0%9D%EC%B2%B4%EC%A7%80%ED%96%A5%20%EC%BF%BC%EB%A6%AC%20%EC%96%B8%EC%96%B4.md)
 
 
 `/jpashop`
---
+++ jpashop/src/main/java/japbook/jpashop/api/MemberApiController.java
@@ -0,0 +1,46 @@
+package japbook.jpashop.api;
+
+import japbook.jpashop.domain.Member;
+import japbook.jpashop.service.MemberService;
+import javax.validation.Valid;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    private CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    private CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
