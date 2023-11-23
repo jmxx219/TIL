c0da9612b8551afb59bec1c21ec6599b14510aa0
Jimin <thswlals0219@gmail.com>
2023-11-23 16:26:57 +0900

    Init: [websocket + stomp] 서버

---
+++ websocket-stomp/src/test/java/com/chat/websocketstomp/WebsocketStompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.websocketstomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
