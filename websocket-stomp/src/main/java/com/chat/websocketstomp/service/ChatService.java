46d1e68ce8ffe9ad38a1be82a3484d2ca1faffe4
Jimin <thswlals0219@gmail.com>
2023-11-23 19:36:15 +0900

    chore: 불필요한 코드 제거

--- websocket-stomp/src/main/java/com/chat/websocketstomp/service/ChatService.java
+++ websocket-stomp/src/main/java/com/chat/websocketstomp/service/ChatService.java
@@ -10,27 +10,13 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final ObjectMapper objectMapper;
     private final ChatRoomRepository chatRoomRepository;
-
-    public <T> void sendMessage(WebSocketSession receiver, ChatMessage message) {
-        try {
-            if(receiver != null && receiver.isOpen()) { // 타켓이 존재하고, 연결된 상태라면 메세지 전송
-                receiver.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
-            }
-        }
-        catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
 
     public ChatRoom createChatRoom(String roomName) {
         String roomId = UUID.randomUUID().toString();
