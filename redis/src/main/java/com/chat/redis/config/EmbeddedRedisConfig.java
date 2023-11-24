3b5ecf581918cc6ae68ba15edba3998896f55c0f
Jimin <thswlals0219@gmail.com>
2023-11-24 14:25:34 +0900

    fix: m1에서 Embedded Redis 실행 오류 해결

--- redis/src/main/java/com/chat/redis/config/EmbeddedRedisConfig.java
+++ redis/src/main/java/com/chat/redis/config/EmbeddedRedisConfig.java
@@ -1,11 +1,17 @@
 package com.chat.redis.config;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.StringUtils;
 import redis.embedded.RedisServer;
 
 /**
@@ -22,8 +28,15 @@ public class EmbeddedRedisConfig {
     private RedisServer redisServer;
 
     @PostConstruct
-    public void redisServer() {
-        redisServer = new RedisServer(redisPort);
+    public void startRedis() throws IOException {
+        int port = isRedisRunning() ? findAvailablePort() : redisPort;
+        if (isArmArchitecture()) {
+            log.info("ARM Architecture");
+            redisServer = new RedisServer(Objects.requireNonNull(getRedisServerExecutable()), port);
+        }
+        else {
+            redisServer = new RedisServer(redisPort);
+        }
         redisServer.start();
     }
 
@@ -32,5 +45,63 @@ public class EmbeddedRedisConfig {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+    /**
+     * Embedded Redis가 현재 실행중인지 확인
+     */
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 현재 PC/서버에서 사용가능한 포트 조회
+     */
+    public int findAvailablePort() throws IOException {
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    /**
+     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
+     */
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    /**
+     * 해당 Process가 현재 실행중인지 확인
+     */
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+        }
+        catch (Exception e) {
+        }
+        return StringUtils.hasText(pidInfo.toString());
+    }
+
+    private File getRedisServerExecutable() {
+        try {
+            return new File("src/main/resources/binary/redis/redis-server-6.2.14-mac-arm64");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean isArmArchitecture() {
+        return System.getProperty("os.arch").contains("aarch64");
     }
 }
--- redis/src/main/java/com/chat/redis/config/RedisConfig.java
+++ redis/src/main/java/com/chat/redis/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.chat.redis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -10,6 +13,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+//    @Value("${spring.data.redis.password}")
+//    private int password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setHostName(host);
+        redisConfiguration.setPort(port);
+//        redisConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisConfiguration);
+    }
 
     /**
      * redis의 pub/sub 메시지를 처리하는 listener 설정
