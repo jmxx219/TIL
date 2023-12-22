0faa59ec73a23737929aafea638f1c7b97569f12
Jimin <thswlals0219@gmail.com>
2023-12-22 17:25:02 +0900

    feat: 회원 수정 api 구현

--- jpashop/src/main/java/japbook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/japbook/jpashop/api/MemberApiController.java
@@ -3,9 +3,12 @@ package japbook.jpashop.api;
 import japbook.jpashop.domain.Member;
 import japbook.jpashop.service.MemberService;
 import javax.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,26 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
     @Data
