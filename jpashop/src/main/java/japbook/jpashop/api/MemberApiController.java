6a06b9e4a41174990366d2fe71a3cab0bf12a468
Jimin <thswlals0219@gmail.com>
2023-12-22 17:49:14 +0900

    feat: 회원 조회 api 구현

--- jpashop/src/main/java/japbook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/japbook/jpashop/api/MemberApiController.java
@@ -2,10 +2,13 @@ package japbook.jpashop.api;
 
 import japbook.jpashop.domain.Member;
 import japbook.jpashop.service.MemberService;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +20,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers(); //엔티티 -> DTO 변환
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     private CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
