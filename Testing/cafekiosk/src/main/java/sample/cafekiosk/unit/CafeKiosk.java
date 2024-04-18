030f39de67407ef924d1da5a589db5e56bb8ae37
Jimin <thswlals0219@gmail.com>
2024-04-18 19:56:43 +0900

    test: 키오스크 주문 테스트 추가

--- Testing/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ Testing/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.unit;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +12,22 @@ import sample.cafekiosk.unit.order.Order;
 @Getter
 public class CafeKiosk {
 
+	private static final LocalTime SHOP_OPEN_TIME = LocalTime.of(10, 0);
+	private static final LocalTime SHOP_CLOSED_TIME = LocalTime.of(22, 0);
+
 	private final List<Beverage> beverages = new ArrayList<>();
 
 	public void add(Beverage beverage) {
 		beverages.add(beverage);
+	}
+
+	public void add(Beverage beverage, int count) {
+		if(count <= 0) {
+			throw new IllegalArgumentException("음료수는 1잔 이상 주문하실 수 있습니다.");
+		}
+		for (int i = 0; i < count; i++) {
+			beverages.add(beverage);
+		}
 	}
 
 	public void remove(Beverage beverage) {
@@ -30,6 +43,21 @@ public class CafeKiosk {
 	}
 
 	public Order createOrder() {
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		LocalTime currentTime = currentDateTime.toLocalTime();
+		if(currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSED_TIME)) {
+			throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+		}
+
+		return new Order(LocalDateTime.now(), beverages);
+	}
+
+	public Order createOrder(LocalDateTime currentDateTime) {
+		LocalTime currentTime = currentDateTime.toLocalTime();
+		if(currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSED_TIME)) {
+			throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+		}
+
 		return new Order(LocalDateTime.now(), beverages);
 	}
 }
