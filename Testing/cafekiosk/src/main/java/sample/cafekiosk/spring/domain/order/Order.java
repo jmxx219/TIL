c374094417399e1467f87ed1815265d87e364086
Jimin <thswlals0219@gmail.com>
2024-05-08 22:29:20 +0900

    test: Mockito로 Sutbbing하기

--- Testing/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ Testing/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -43,8 +44,9 @@ public class Order extends BaseEntity {
 	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderProduct> orderProducts = new ArrayList<>();
 
-	public Order(List<Product> products, LocalDateTime registeredDateTime) {
-		this.orderStatus = OrderStatus.INIT;
+	@Builder
+	private Order(List<Product> products, OrderStatus orderStatus, LocalDateTime registeredDateTime) {
+		this.orderStatus = orderStatus;
 		this.totalPrice = calculateTotalPrice(products);
 		this.registeredDateTime = registeredDateTime;
 		this.orderProducts = products.stream()
@@ -53,7 +55,11 @@ public class Order extends BaseEntity {
 	}
 
 	public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
-		return new Order(products, registeredDateTime);
+		return Order.builder()
+			.orderStatus(OrderStatus.INIT)
+			.products(products)
+			.registeredDateTime(registeredDateTime)
+			.build();
 	}
 
 	private int calculateTotalPrice(List<Product> products) {
--- Testing/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ Testing/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -43,7 +43,7 @@ class ProductServiceTest {
 		Product product = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
 		productRepository.save(product);
 
-		ProductCreateRequest request = ProductCreateRequest.builder()
+		ProductCreateServiceRequest request = ProductCreateServiceRequest.builder()
 			.type(HANDMADE)
 			.sellingStatus(SELLING)
 			.name("카푸치노")
@@ -51,7 +51,7 @@ class ProductServiceTest {
 			.build();
 
 		// when
-		ProductResponse productResponse = productService.createProduct(request.toServiceRequest());
+		ProductResponse productResponse = productService.createProduct(request);
 
 		// then
 		assertThat(productResponse)
@@ -71,7 +71,7 @@ class ProductServiceTest {
 	@Test
 	void createProductWhenProductsIsEmpty() throws Exception {
 		// given
-		ProductCreateRequest request = ProductCreateRequest.builder()
+		ProductCreateServiceRequest request = ProductCreateServiceRequest.builder()
 			.type(HANDMADE)
 			.sellingStatus(SELLING)
 			.name("카푸치노")
@@ -79,7 +79,7 @@ class ProductServiceTest {
 			.build();
 
 		// when
-		ProductResponse productResponse = productService.createProduct(request.toServiceRequest());
+		ProductResponse productResponse = productService.createProduct(request);
 
 		// then
 		assertThat(productResponse)
